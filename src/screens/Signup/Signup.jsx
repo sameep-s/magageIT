aba260b3c3c82f7ab343312a3d221fc3357a86cc
sameep-s <sameep.sharma98@gmail.com>
2023-04-01 10:06:23 +0530

    Added: inner container to signup

--- src/screens/Signup/Signup.jsx
+++ src/screens/Signup/Signup.jsx
@@ -22,47 +22,49 @@ export function Signup() {
             <Navbar />
             <div className="wrapper__main__signup">
                 <div className="container__main__signup">
-                    <form onSubmit={(e) => handleSignup(e, signupData, setSignupData, defaultData)}>
-                        <div className="form__title">Signup</div>
-                        <div className="">
-                            <input
-                                value={signupData.uname}
-                                type="text"
-                                onChange={(e) => setSignupData({ ...signupData, uname: e.target.value })}
-                                placeholder="Enter Username"
-                            />
-                        </div>
-                        <div className="">
-                            <input
-                                value={signupData.email}
-                                type="text"
-                                placeholder="Enter Your Email"
-                                onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
-                            />
-                        </div>
-                        <div className="password">
-                            <input
-                                value={signupData.password}
-                                type={passwordVisible ? "text" : "password"}
-                                placeholder="Enter password"
-                                onChange={(e) => setSignupData({ ...signupData, password: e.target.value })}
-                            />
-                            <span className="passwordToggle" onClick={() => setPasswordVisible(prev => !prev)}>{!passwordVisible ? <EyeOff /> : <Eye />}</span>
-                        </div>
-                        <div className="password">
-                            <input
-                                value={signupData.confiPassword}
-                                type={passwordVisible ? "text" : "password"}
-                                placeholder="Confirm password"
-                                onChange={(e) => setSignupData({ ...signupData, confiPassword: e.target.value })}
-                            />
-                            <span className="passwordToggle" onClick={() => setPasswordVisible(prev => !prev)}>{!passwordVisible ? <EyeOff /> : <Eye />}</span>
-                        </div>
-                        <div className="action">
-                            <button type="submit">Signup</button>
-                        </div>
-                        <div className="classEndPara">Already a user? <Link to={'/login'}><span>Login</span></Link></div>
-                    </form>
+                    <div className="container__inner">
+                        <form onSubmit={(e) => handleSignup(e, signupData, setSignupData, defaultData)}>
+                            <div className="form__title">Signup</div>
+                            <div className="">
+                                <input
+                                    value={signupData.uname}
+                                    type="text"
+                                    onChange={(e) => setSignupData({ ...signupData, uname: e.target.value })}
+                                    placeholder="Enter Username"
+                                />
+                            </div>
+                            <div className="">
+                                <input
+                                    value={signupData.email}
+                                    type="text"
+                                    placeholder="Enter Your Email"
+                                    onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
+                                />
+                            </div>
+                            <div className="password">
+                                <input
+                                    value={signupData.password}
+                                    type={passwordVisible ? "text" : "password"}
+                                    placeholder="Enter password"
+                                    onChange={(e) => setSignupData({ ...signupData, password: e.target.value })}
+                                />
+                                <span className="passwordToggle" onClick={() => setPasswordVisible(prev => !prev)}>{!passwordVisible ? <EyeOff /> : <Eye />}</span>
+                            </div>
+                            <div className="password">
+                                <input
+                                    value={signupData.confiPassword}
+                                    type={passwordVisible ? "text" : "password"}
+                                    placeholder="Confirm password"
+                                    onChange={(e) => setSignupData({ ...signupData, confiPassword: e.target.value })}
+                                />
+                                <span className="passwordToggle" onClick={() => setPasswordVisible(prev => !prev)}>{!passwordVisible ? <EyeOff /> : <Eye />}</span>
+                            </div>
+                            <div className="action">
+                                <button type="submit">Signup</button>
+                            </div>
+                            <div className="classEndPara">Already a user? <Link to={'/login'}><span>Login</span></Link></div>
+                        </form>
+                    </div>
                 </div >
             </div >
         </>
